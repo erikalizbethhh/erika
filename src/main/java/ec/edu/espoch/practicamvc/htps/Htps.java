14cf8c2f02b7d9b8b663e3ebfbd4edae7c710197
erikalizbethhhhhhhhh@gmail.com <tuemail@ejemplo.com>
2024-11-26 08:17:49 -0500

    erika lizbeth

---
+++ src/main/java/ec/edu/espoch/practicamvc/htps/Htps.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.practicamvc.htps;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Htps {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
